1ddd6c7359ef6cb0e63a45314da8d92370ce18a6
eray.soydan <eray.soydan@foriba.com>
2022-06-06 15:21:42 +0300

    OrderManagementContext.cs düzenlendi.

--- OrderManagement.API/Models/Entity/OrderManagementContext.cs
+++ OrderManagement.API/Models/Entity/OrderManagementContext.cs
@@ -20,15 +20,6 @@ namespace OrderManagement.API.Models.Entity
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<OrderStatus> OrderStatuses { get; set; }
 
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=OrderManagement;Trusted_Connection=True;");
-//            }
-//        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Item>(entity =>
